bfa69b363c69d75659a866e25c16cea8df96a14d
Jason Cheatham <j.cheatham@gmail.com>
2019-09-22 16:33:04 -0400

    Make startship HEAD point to 'npm' branch

--- HomebrewFormula/starship.rb
+++ HomebrewFormula/starship.rb
@@ -1,8 +1,11 @@
 class Starship < Formula
   desc "The cross-shell prompt for astronauts (jason0x43's fork)"
   homepage "https://starship.rs"
+
   url "https://github.com/jason0x43/starship/archive/v0.18.0.tar.gz"
-  head "https://github.com/jason0x43/starship.git"
+  sha256 "6792e7359577eca6f858559ebbee80dee4ccb97039973f8ba6f14a05c6e38c13"
+
+  head "https://github.com/jason0x43/starship.git", :branch => "npm"
 
   depends_on "rust" => :build
 
