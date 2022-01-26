94e32249c6393d5aa04b2d36c620a6f3ed887c07
Jason Cheatham <jason@jasoncheatham.com>
2022-01-26 12:40:00 -0500

    Add formula for watchman@2021.11.29

---
+++ Formula/watchman@2021.11.29.rb
@@ -0,0 +1,83 @@
+class Watchman < Formula
+  desc "Watch files and take action when they change"
+  homepage "https://github.com/facebook/watchman"
+  url "https://github.com/facebook/watchman/archive/v2021.11.29.00.tar.gz"
+  sha256 "aed83c9369ecdb6094ea836a312ff0eb95b09d929c1ba3e256ac78a625a8a15a"
+  license "MIT"
+  head "https://github.com/facebook/watchman.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any, arm64_monterey: "a59de4c61d10a2217839f97bd9d239d9db3453c6eac3e21a62a20ecb6811b549"
+    sha256 cellar: :any, arm64_big_sur:  "63ea29e0a299a899d154beedde13b71566ce7754ea186ec717a65fb0aa685f99"
+    sha256 cellar: :any, monterey:       "1b53c1dd613a5a65a49b3afb6b733c242e16170810a7ef3c03303aca2a0ea03d"
+    sha256 cellar: :any, big_sur:        "0476f53db47bdb4761749b1de79c3365fe0bb66c7f496bb205b5c0a8c135a8d9"
+    sha256 cellar: :any, catalina:       "6237e7163106ac456a16a5de92544905444f50b9a8244848ecd5992e919753ac"
+    sha256               x86_64_linux:   "8fb9303967970fc10abc01d0846779bcd5667cc810a11a657fd82d308a0ec8f6"
+  end
+
+  # https://github.com/facebook/watchman/issues/963
+  pour_bottle? only_if: :default_prefix
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "rust" => :build
+  depends_on "boost"
+  depends_on "fmt"
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "libevent"
+  depends_on "openssl@1.1"
+  depends_on "pcre"
+  depends_on "python@3.10"
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  # The `googletest` formula (v1.11+) currently causes build failures.
+  # On macOS: watchman_string.h:114:16: error: no member named 'data' in 'watchman_pending_fs'
+  # On Linux: gtest-printers.h:211:33: error: no match for 'operator<<'
+  # Use https://github.com/facebook/watchman/blob/#{version}/build/fbcode_builder/manifests/googletest
+  resource "googletest" do
+    url "https://github.com/google/googletest/archive/release-1.10.0.tar.gz"
+    sha256 "9dc9157a9a1551ec7a7e43daea9a694a0bb5fb8bec81235d8a1e6ef64c716dcb"
+  end
+
+  def install
+    resource("googletest").stage do
+      cmake_args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] }
+      system "cmake", ".", *cmake_args, "-DCMAKE_INSTALL_PREFIX=#{buildpath}/googletest"
+      system "make", "install"
+    end
+    ENV["GTest_DIR"] = ENV["GMock_DIR"] = buildpath/"googletest"
+
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DENABLE_EDEN_SUPPORT=OFF",
+                    "-DWATCHMAN_VERSION_OVERRIDE=#{version}",
+                    "-DWATCHMAN_BUILDINFO_OVERRIDE=#{tap.user}",
+                    "-DWATCHMAN_STATE_DIR=#{var}/run/watchman",
+                    *std_cmake_args
+
+    # Workaround for `Process terminated due to timeout`
+    ENV.deparallelize { system "cmake", "--build", "build" }
+    system "cmake", "--install", "build"
+
+    path = Pathname.new(File.join(prefix, HOMEBREW_PREFIX))
+    bin.install Dir[path/"bin/*"]
+    lib.install Dir[path/"lib/*"]
+    path.rmtree
+  end
+
+  def post_install
+    (var/"run/watchman").mkpath
+    chmod 042777, var/"run/watchman"
+  end
+
+  test do
+    assert_equal(version.to_s, shell_output("#{bin}/watchman -v").chomp)
+  end
+end
